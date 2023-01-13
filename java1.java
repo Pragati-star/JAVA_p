e1393f3190f73f04351955026a8ec5c29fe674d3
Pragati <pragati2004muz@gmail.com>
2023-01-13 09:29:20 +0530

    commit1

---
+++ java1.java
@@ -0,0 +1,10 @@
+class day;
+public static void main(String[] args) {
+    // for(int i=1; i<=100; i++) {
+       // System.out.println("Hello World");
+    //}
+    int n = 20;
+    for(int i=1; i<=n; i++) {
+        System.out.println();
+    }
+}
